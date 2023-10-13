4ba4d61c7079cec6bbec9b27b54e43400285dc7a
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2023-10-13 12:10:55 +0500

    Ползовател показат исправлено

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -39,17 +39,8 @@ class UsersController extends Controller
         $personal_info->last_name = $request->last_name;
         $personal_info->middle_name = $request->middle_name;
         $personal_info->phone_number = $request->phone_number;
-        $letters = range('a', 'z');
-        $random_array = [$letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)]];
-        $random = implode("", $random_array);
         $file = $request->file('avatar');
-
-        if (isset($file)) {
-            $image_name = $random . '' . date('Y-m-dh-i-s') . '.' . $file->extension();
-            $file->storeAs('public/user/', $image_name);
-            $personal_info->avatar = $image_name;
-        }
-
+        $this->imageSave($file, $personal_info, 'store');
         $personal_info->gender = $request->gender;
         $personal_info->birth_date = $request->birth_date;
         $personal_info->save();
@@ -71,8 +62,31 @@ class UsersController extends Controller
     public function show(string $id)
     {
         $model = User::find($id);
+        if(isset($model->personalInfo->birth_date)){
+            $birth_date_array = explode(' ', $model->personalInfo->birth_date);
+            $now_time = strtotime('now');
+            $birth_time = strtotime($birth_date_array[0]);
+            $month = date('m', ($now_time));
+            $day = date('d', ($now_time));
+            $birth_month = date('m', ($birth_time));
+            $birth_date = date('d', ($birth_time));
+            $year = date('Y', ($now_time));
+            $birth_year = date('Y', ($birth_time));
+            $year_old = 0;
+            if($year > $birth_year){
+                $year_old = $year - $birth_year - 1;
+                if($month > $birth_month){
+                    $year_old = $year_old +1;
+                }elseif($month == $birth_month){
+                    if($day >= $birth_date){
+                        $year_old = $year_old +1;
+                    }
+                }
+            }
+        }
         return view('admin.user.show', [
-            'model' => $model
+            'model' => $model,
+            'year_old' => $year_old
         ]);
     }
 
@@ -99,19 +113,8 @@ class UsersController extends Controller
             $personal_info->last_name = $request->last_name;
             $personal_info->middle_name = $request->middle_name;
             $personal_info->phone_number = $request->phone_number;
-            $letters = range('a', 'z');
-            $random_array = [$letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)]];
-            $random = implode("", $random_array);
             $file = $request->file('avatar');
-            if (isset($file)) {
-                $sms_avatar = storage_path('app/public/user/' . $personal_info->avatar);
-                if (file_exists($sms_avatar)) {
-                    unlink($sms_avatar);
-                }
-                $image_name = $random.''.date('Y-m-dh-i-s').'.'.$file->extension();
-                $file->storeAs('public/user/', $image_name);
-                $personal_info->avatar = $image_name;
-            }
+            $this->imageSave($file, $personal_info, 'update');
             $personal_info->gender = $request->gender;
             $personal_info->birth_date = $request->birth_date;
             $personal_info->save();
@@ -121,17 +124,8 @@ class UsersController extends Controller
             $personal_info->last_name = $request->last_name;
             $personal_info->middle_name = $request->middle_name;
             $personal_info->phone_number = $request->phone_number;
-            $letters = range('a', 'z');
-            $random_array = [$letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)]];
-            $random = implode("", $random_array);
             $file = $request->file('avatar');
-
-            if (isset($file)) {
-                $image_name = $random . '' . date('Y-m-dh-i-s') . '.' . $file->extension();
-                $file->storeAs('public/user/', $image_name);
-                $personal_info->avatar = $image_name;
-            }
-
+            $this->imageSave($file, $personal_info, 'store');
             $personal_info->gender = $request->gender;
             $personal_info->birth_date = $request->birth_date;
             $personal_info->save();
@@ -146,14 +140,43 @@ class UsersController extends Controller
         return redirect()->route('user.index')->with('status', __('Successfully updated'));
     }
 
+    public function imageSave($file, $personal_info, $text){
+
+        if (isset($file)) {
+            $letters = range('a', 'z');
+            $random_array = [$letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)], $letters[rand(0,25)]];
+            $random = implode("", $random_array);
+
+            if($text == 'update'){
+                if(isset($personal_info->avatar)){
+                    $sms_avatar = storage_path('app/public/user/' . $personal_info->avatar);
+                }else{
+                    $sms_avatar = storage_path('app/public/user/' . 'no');
+                }
+                if (file_exists($sms_avatar)) {
+                    unlink($sms_avatar);
+                }
+            }
+            $image_name = $random.''.date('Y-m-dh-i-s').'.'.$file->extension();
+            $file->storeAs('public/user/', $image_name);
+            $personal_info->avatar = $image_name;
+
+            return $personal_info;
+        }
+    }
+
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
         $model = User::find($id);
-        if (isset($model->avatar)) {
-            $sms_avatar = storage_path('app/public/user/'.$model->avatar);
+        if (isset($model->personalInfo->id)) {
+            $model->personalInfo->delete();
+        }
+
+        if(isset($model->personalInfo->avatar)) {
+            $sms_avatar = storage_path('app/public/user/'.$model->personalInfo->avatar);
         } else {
             $sms_avatar = 'no';
         }
--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -24,45 +24,27 @@
                 <div class="profile_box">
                     <div class="d-flex align-items-start">
                         <div>
-                            @if(isset($model))
+                            @if(isset($model->personalInfo->id))
                                 @php
-                                    $sms_avatar = storage_path('app/public/user/'.$model->avatar??'no');
-                                @endphp
-                                @php
-                                if(isset($model->birth_date)){
-                                    $birth_date_array = explode(' ', $model->birth_date);
-                                    $now_time = strtotime('now');
-                                    $birth_time = strtotime($model->birth_date);
-                                    $month = date('m', ($now_time));
-                                    $day = date('d', ($now_time));
-                                    $birth_month = date('m', ($birth_time));
-                                    $birth_date = date('d', ($birth_time));
-                                    $year = date('Y', ($now_time));
-                                    $birth_year = date('Y', ($birth_time));
-                                    $year_old = 0;
-                                    if($year > $birth_year){
-                                        $year_old = $year - $birth_year - 1;
-                                        if($month > $birth_month){
-                                            $year_old = $year_old +1;
-                                        }elseif($month == $birth_month){
-                                            if($day >= $birth_date){
-                                                $year_old = $year_old +1;
-                                            }
-                                        }
-                                    }
-                                }
+                                    $sms_avatar = storage_path('app/public/user/'.$model->personalInfo->avatar??'no');
                                 @endphp
                                 @if(file_exists($sms_avatar))
-                                    <img class="user_photo_2" src="{{asset('storage/user/'.$model->avatar)}}" alt="">
+                                    <img class="user_photo_2" src="{{asset('storage/user/'.$model->personalInfo->avatar)}}" alt="">
                                 @else
                                     <img class="user_photo_2" src="{{asset('assets/images/man.jpg')}}" alt="">
                                 @endif
                             @endif
                         </div>
                         <div id="color_black" style="margin-left: 30px;">
-                            <h3 >@if(isset($model)){{$model->first_name.' '.$model->last_name.' '.$model->middle_name}}@endif</h3>
-                            <p>{{__('Role').': '}}<b>{{$model->is_admin == 1?'Admin':'User' }}</b></p>
-                            <p>{{__('Phone').': '}}<b>@if(isset($model)){{$model->phone_number??''}}@endif</b></p>
+                            <h3 >@if(isset($model->personalInfo->id)){{$model->personalInfo->first_name.' '.$model->personalInfo->last_name.' '.$model->personalInfo->middle_name}}@endif</h3>
+                            <p>{{__('Role').': '}}
+                                <b>
+                                    @if(isset($model->role_id))
+                                        @if($model->role_id == 0){{ __('Stuff') }}@elseif($model->role_id == 1)  {{ __('Seller') }} @elseif($model->role_id == 2) {{ __('User') }} @else <div class="no_text"></div> @endif
+                                    @endif
+                                </b>
+                            </p>
+                            <p>{{__('Phone').': '}}<b>{{$model->phone_number??''}}</b></p>
                             <p>{{__('Age').': '}}<b>{{$year_old??''}}</b></p>
                         </div>
                     </div>
@@ -79,7 +61,9 @@
                             <div class="d-flex justify-content-between" style="margin-top: 20px; align-items: center">
                                 <h3 class="text_name">{{__('Role')}}:</h3>
                                 <div class="text_value">
-                                    {{$model->is_admin == 1?'Admin':'User' }}
+                                    @if(isset($model->role_id))
+                                        @if($model->role_id == 0){{ __('Stuff') }}@elseif($model->role_id == 1)  {{ __('Seller') }} @elseif($model->role_id == 2) {{ __('User') }} @else <div class="no_text"></div> @endif
+                                    @endif
                                 </div>
                             </div>
 
@@ -97,16 +81,16 @@
                             <div class="d-flex justify-content-between" style="align-items: center">
                                 <h3 class="text_name">{{__('Full Name')}}:</h3>
                                 <div class="text_value">
-                                    @if(isset($model))
-                                        {{$model->first_name.' '.$model->last_name.' '.$model->middle_name}}
+                                    @if(isset($model->personalInfo->id))
+                                        {{$model->personalInfo->first_name.' '.$model->personalInfo->last_name.' '.$model->personalInfo->middle_name}}
                                     @endif
                                 </div>
                             </div>
                             <div class="d-flex justify-content-between" style="margin-top: 20px; align-items: center">
                                 <h3 class="text_name">{{__('Birth date')}}:</h3>
                                 @php
-                                    if(isset($model) && isset($model->birth_date)){
-                                            $birth_date_arr = explode(' ', $model->birth_date);
+                                    if(isset($model->personalInfo->birth_date)){
+                                            $birth_date_arr = explode(' ', $model->personalInfo->birth_date);
                                     }else{
                                         $birth_date_arr = [];
                                     }
