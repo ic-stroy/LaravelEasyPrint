de09b59b93f9bafb799e72ed5c4aeee39095a43c
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2023-10-26 10:58:32 +0500

    компания инфо показания виполнено. адресс компании покажется через яндекс карта

--- resources/views/admin/company/show.blade.php
+++ resources/views/admin/company/show.blade.php
@@ -4,6 +4,13 @@
     {{-- Your page title --}}
 @endsection
 @section('content')
+    <style>
+        .yandex_maps{
+            height: 400px;
+            min-width: 400px;
+            max-width: 600px;
+        }
+    </style>
     <div class="card">
         <div class="card-body">
             <h4 class="mt-0 header-title">{{__('Color lists')}}</h4>
@@ -23,8 +30,24 @@
                         <td>{{$model->name??''}}</td>
                     </tr>
                     <tr>
-                        <th>{{__('Color')}}</th>
-                        <td><div style="background-color: {{$model->code??''}}; height: 40px; width: 64px; border-radius: 4px; border: solid 1px"></div></td>
+                        <th>{{__('Delivery price')}}</th>
+                        <td>{{$model->delivery_price??''}}</td>
+                    </tr>
+                    <tr>
+                        <th>{{__('Region')}}</th>
+                        <td>{{$model->address->region??''}}</td>
+                    </tr>
+                    <tr>
+                        <th>{{__('District')}}</th>
+                        <td>{{$model->address->district??''}}</td>
+                    </tr>
+                    <tr>
+                        <th>{{__('Map')}}</th>
+                        <td>
+                            <div class="yandex_maps" id="map">
+
+                            </div>
+                        </td>
                     </tr>
                     <tr>
                         <th>{{__('Updated at')}}</th>
@@ -34,4 +57,32 @@
             </table>
         </div>
     </div>
+    <script src="https://api-maps.yandex.ru/2.1/?apikey=ваш API-ключ&lang=ru_RU"></script>
+    <script>
+        @if(isset($model->address->latitude) && isset($model->address->longitude))
+            let center = ["{{$model->address->latitude}}", "{{$model->address->longitude}}"]
+        @else
+            let center = [41.32508826970849, 69.32866686970848]
+        @endif
+        function init() {
+            let map = new ymaps.Map('map', {
+                center: center,
+                zoom: 17
+            });
+
+            let placemark = new ymaps.Placemark(center, {}, {});
+
+            map.controls.remove('geolocationControl'); // удаляем геолокацию
+            map.controls.remove('searchControl'); // удаляем поиск
+            map.controls.remove('trafficControl'); // удаляем контроль трафика
+            // map.controls.remove('typeSelector'); // удаляем тип
+            map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+            map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+            map.controls.remove('rulerControl'); // удаляем контрол правил
+            // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+            map.geoObjects.add(placemark);
+        }
+        ymaps.ready(init);
+    </script>
 @endsection
