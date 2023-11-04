f3f94112610e0878cca033121acbf03281a78037
Alijon0185 <muxtorovalijon0127@gmail.com>
2023-11-04 15:39:33 +0500

    added cities table

---
+++ database/migrations/2023_06_08_065102_create_yy_cities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('yy_cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('type')->nullable();
+            $table->integer('parent_id')->nullable();
+            $table->double('distance')->nullable();
+            $table->double('lat')->nullable();
+            $table->double('lng')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('yy_cities');
+    }
+};
