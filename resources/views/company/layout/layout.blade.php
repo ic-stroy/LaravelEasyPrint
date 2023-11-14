87ccc065f36fd8af3c2605673fa7f6ccf069f9c3
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2023-11-14 14:58:12 +0500

    титле изменено

--- resources/views/company/layout/layout.blade.php
+++ resources/views/company/layout/layout.blade.php
@@ -7,7 +7,7 @@
 @endphp
 <head>
     <meta charset="utf-8" />
-    <title>{{ $title ?? 'Easy print' }}</title>
+    <title>{{ $title ?? 'Easy Print' }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
     <meta content="Coderthemes" name="author" />
