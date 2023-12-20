00bb9d6f14d6c54a966d9fe9257e35905fc3f675
Alijon0185 <muxtorovalijon0127@gmail.com>
2023-12-20 15:24:15 +0500

    $requestdan kelgan malumotlar tekshirildi

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -592,7 +592,7 @@ class OrderController extends Controller
         $order_inner=$data['data'];
         // dd($data['data']);
         $order_id=$data['order_id'];
-
+        return response()->json($data, 200);
         if ($order_id  && $order=Order::where('id',$order_id)->where('status', Constants::BASKED)->first()) {
             $order_price=0;
             $order_discount_price=0;
