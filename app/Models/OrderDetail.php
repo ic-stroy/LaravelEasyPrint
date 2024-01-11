824122a4607c0fd5100fb406f83d6c59b0f378aa
Alijon777 <alijoneasy@gmail.com>
2024-01-11 13:15:42 +0300

    soft_delete qo'shildi

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OrderDetail extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $table = 'order_details';
 
