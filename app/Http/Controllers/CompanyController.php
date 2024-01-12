9bf1167483360beb6269d9d2593de1d6a2bfcd89
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2024-01-12 11:23:32 +0500

    Города исправлено

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -28,24 +28,34 @@ class CompanyController extends Controller
      */
     public function create(Request $request)
     {
-//        $response = Http::get(asset("assets/json/cities.json"));
-//        $cities = json_decode($response);
-//        foreach ($cities as $city){
-//            $model_region = new Cities();
-//            $model_region->name = $city->region;
-//            $model_region->type = 'region';
-//            $model_region->parent_id = 0;
-//            $model_region->save();
-//            foreach ($city->cities as $city_district){
-//                $model = new Cities();
-//                $model->name = $city_district->name;
-//                $model->type = 'district';
-//                $model->parent_id = $model_region->id;
-//                $model->lng = $city_district->long;
-//                $model->lat = $city_district->lat;
-//                $model->save();
-//            }
-//        }
+        $response = Http::get(asset("assets/json/cities.json"));
+        $cities = json_decode($response);
+        foreach ($cities as $city){
+            if(!Cities::where('name', $city->region)->exists()){
+                $model_region = new Cities();
+                $model_region->name = $city->region;
+                $model_region->type = 'region';
+                $model_region->parent_id = 0;
+                $model_region->lng = $city->long;
+                $model_region->lat = $city->lat;
+                $model_region->save();
+            foreach ($city->cities as $city_district){
+                $model = new Cities();
+                $model->name = $city_district->name;
+                $model->type = 'district';
+                $model->parent_id = $model_region->id;
+                $model->lng = $city_district->long;
+                $model->lat = $city_district->lat;
+                $model->save();
+            }
+            }else{
+                $model_region = Cities::where('name', $city->region)->first();
+                $model_region->lng = $city->long;
+                $model_region->lat = $city->lat;
+                $model_region->save();
+            }
+
+        }
          return view('admin.company.create');
     }
 
