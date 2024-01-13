ff0ad2025d15463f59e317a04b52b10cd71be19b
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2024-01-13 14:16:14 +0500

    купон обновления исправлено

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -90,18 +90,10 @@ class CouponController extends Controller
         if(isset($request->company_id)){
             $coupon->company_id = $request->company_id;
         }
-        if(isset($request->order_count)){
-            $coupon->order_count = $request->order_count;
-        }
-        if(isset($request->type)){
-            $coupon->type = $request->type;
-        }
-        if(isset($request->start_date)){
-            $coupon->start_date = $request->start_date;
-        }
-        if(isset($request->end_date)){
-            $coupon->end_date = $request->end_date;
-        }
+        $coupon->order_count = $request->order_count;
+        $coupon->type = $request->type;
+        $coupon->start_date = $request->start_date;
+        $coupon->end_date = $request->end_date;
         $coupon->save();
         return redirect()->route('coupons.index')->with('status', translate('Successfully created'));
     }
