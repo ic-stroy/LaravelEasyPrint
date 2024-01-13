2a5023ecda3a6b98e39cc86f3bdadf996d0074c8
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2024-01-13 13:01:31 +0500

    Купон создания выполнено

--- resources/views/admin/coupons/create.blade.php
+++ resources/views/admin/coupons/create.blade.php
@@ -22,34 +22,28 @@
                 @csrf
                 @method("POST")
                 <div class="row">
-                    <div class="mb-3 col-6">
+                    <div class="mb-3 col-4">
                         <label class="form-label">{{translate('Coupon name')}}</label>
                         <input type="text" name="name" class="form-control" required value="{{old('name')}}"/>
                     </div>
-                    <div class="mb-3 col-6">
+                    <div class="mb-3 col-4">
                         <label class="form-label">{{translate('Coupon type')}}</label>
-                        <select name="coupon_type" class="form-control" id="coupon_type">
+                        <select name="coupon_type" class="form-control" id="coupon_type" required>
                             <option value="price" class="form-control">{{translate('Price')}}</option>
                             <option value="percent" class="form-control">{{translate('Percent')}}</option>
                         </select>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="mb-3 col-6" id="coupon_price">
+                    <div class="mb-3 col-4" id="coupon_price">
                         <label class="form-label">{{translate('Coupon price')}}</label>
-                        <input type="number" name="price" class="form-control" id="coupon_price_input"  min="0" value="{{old('price')}}"/>
+                        <input type="number" name="price" class="form-control" id="coupon_price_input" min="0" value="{{old('price')}}"/>
                     </div>
-                    <div class="mb-3 col-6 display-none" id="coupon_percent">
+                    <div class="mb-3 col-4 display-none" id="coupon_percent">
                         <label class="form-label">{{translate('Coupon percent')}}</label>
                         <input type="number" name="percent" class="form-control" id="coupon_percent_input" step="0.01" min="0" max="100" value="{{old('percent')}}"/>
                     </div>
-                    <div class="mb-3 col-6">
-                        <label class="form-label">{{translate("Order's min price")}}</label>
-                        <input type="number" name="min_price" class="form-control" min="0" value="{{old('min_price')}}"/>
-                    </div>
                 </div>
                 <div class="row">
-                    <div class="mb-3 col-6">
+                    <div class="mb-3 col-2">
                         <label class="form-label">{{translate('Companies')}}</label>
                         <select name="company_id" class="form-control" id="company_id">
                             <option value="" selected>{{translate('All company')}}</option>
@@ -58,17 +52,19 @@
                             @endforeach
                         </select>
                     </div>
-                    <div class="mb-3 col-6">
+                    <div class="mb-3 col-3">
+                        <label class="form-label">{{translate("Order's min price")}}</label>
+                        <input type="number" name="min_price" class="form-control" min="0" value="{{old('min_price')}}"/>
+                    </div>
+                    <div class="mb-3 col-3">
                         <label class="form-label">{{translate('Number of orders')}}</label>
                         <input type="number" name="order_count" class="form-control"/>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="mb-3 col-6">
+                    <div class="mb-3 col-2">
                         <label class="form-label">{{translate('Start date')}}</label>
                         <input type="date" name="start_date" class="form-control" required value="{{old('start_date')}}"/>
                     </div>
-                    <div class="mb-3 col-6">
+                    <div class="mb-3 col-2">
                         <label class="form-label">{{translate('End date')}}</label>
                         <input type="date" name="end_date" class="form-control" required value="{{old('end_date')}}"/>
                     </div>
