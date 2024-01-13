cb7962b283babb9a8ff6745cc26e1ba78e072b57
Alijon777 <alijoneasy@gmail.com>
2024-01-13 12:51:51 +0500

    coupons tablitsaga type qo'shildi

--- app/Constants.php
+++ app/Constants.php
@@ -22,8 +22,8 @@ class Constants {
 
    // coupon type
    const TO_ORDER_COUNT = 0; //maslan 10 ta ordarga
-   const FOR_ORDER_COUNT = 1; // masalan 10 - ordarga
-   
+   const FOR_ORDER_NUMBER = 1; // masalan 10 - ordarga  // agar typy null bosa demak coupon order countga berilmagan
+
     //Personal info
     const MALE = 1;
     const FEMALE = 2;
