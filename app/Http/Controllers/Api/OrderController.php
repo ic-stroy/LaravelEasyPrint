fe89030f006f7e1558564b8ee251f5da16591bb5
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2024-01-22 14:33:13 +0500

    Order getBasket tuzatildi

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -199,14 +199,11 @@ class OrderController extends Controller
      */
     public function getBasket(Request $request){
         $user = Auth::user();
-
         $language=$request->language;
         if ($language == null) {
             $language=env("DEFAULT_LANGUAGE", 'ru');
         }
-
-        // dd($user->orderBasket->orderDetail);
-        $order=$user->orderBasket;
+        $order = $user->orderBasket;
         $order_detail_list = [];
         foreach($user->orderBasket as $orderBasket){
             if($orderBasket->orderDetail){
@@ -225,95 +222,6 @@ class OrderController extends Controller
                                 'dt4.id as color_id','dt4.name as color_name','dt4.code as color_code','dt5.name as company_name')
                             ->first();
 
-                        // $sizes = DB::table('warehouses as dt1')
-                        //     ->join('sizes as dt3', 'dt3.id', '=', 'dt1.size_id')
-                        //     // ->join('colors as dt4', 'dt4.id', '=', 'dt2.color_id')
-                        //     ->where('dt1.product_id', $warehouse_product->product_id)
-                        //     ->where('dt1.company_id', $warehouse_product->company_id)
-                        //     ->select('dt1.id as id','dt3.id as size_id', 'dt3.name as size_name')
-                        //     ->distinct('size_id')
-                        //     ->get();
-                        //     // dd($sizes);
-
-                        //     $size_list=[];
-                        //     foreach ($sizes as $size) {
-                        //         $colors = DB::table('warehouses as dt1')
-                        //             ->join('colors as dt4', 'dt4.id', '=', 'dt1.color_id')
-                        //             ->where('dt1.product_id', $warehouse_product->product_id)
-                        //             ->where('dt1.company_id', $warehouse_product->company_id)
-                        //             ->where('dt1.size_id', $size->size_id)
-                        //             ->select('dt4.id as color_id','dt4.code as color_code', 'dt4.name as color_name','dt1.images as images')
-                        //             ->distinct('color_id')
-                        //             ->get();
-                        //             // dd($colors);
-
-                        //             $color_list=[];
-                        //             foreach ($colors as $color) {
-                        //                 $aa_color=[
-                        //                     'id'=>$color->color_id,
-                        //                     'code'=>$color->color_code,
-                        //                     'name'=>$color->color_name,
-                        //                 ];
-                        //                 // dd($aa_color);
-                        //                 array_push($color_list,$aa_color);
-                        //             }
-                        //             // dd($color_list);
-
-                        //             $aa_size=[
-                        //                 'id'=>$size->size_id,
-                        //                 'name'=>$size->size_name,
-                        //                 'color'=>$color_list
-                        //             ];
-                        //             array_push($size_list,$aa_size);
-
-                        //             // dd($colors);
-                        //     }
-                        //     // dd($size_list);
-
-                        // $colors = DB::table('warehouses as dt1')
-                        //     ->join('colors as dt3', 'dt3.id', '=', 'dt1.color_id')
-                        //     // ->join('colors as dt4', 'dt4.id', '=', 'dt2.color_id')
-                        //     ->where('dt1.product_id', $warehouse_product->product_id)
-                        //     ->where('dt1.company_id', $warehouse_product->company_id)
-                        //     ->select('dt1.id as id','dt3.id as color_id','dt3.code as color_code', 'dt3.name as color_name')
-                        //     ->distinct('color_id')
-                        //     ->get();
-                        //     // dd($color);
-
-                        //     $aaa_color_list=[];
-                        //     foreach ($colors as $color) {
-                        //         $sizes = DB::table('warehouses as dt1')
-                        //             ->join('sizes as dt4', 'dt4.id', '=', 'dt1.size_id')
-                        //             ->where('dt1.product_id', $warehouse_product->product_id)
-                        //             ->where('dt1.company_id', $warehouse_product->company_id)
-                        //             ->where('dt1.color_id', $color->color_id)
-                        //             ->select('dt4.id as size_id','dt4.name as size_name')
-                        //             ->distinct('size_id')
-                        //             ->get();
-                        //             // dd($sizes);
-
-                        //             $aaa_size_list=[];
-                        //             foreach ($sizes as $size) {
-                        //                 $aas_size=[
-                        //                     'id'=>$size->size_id,
-                        //                     'name'=>$size->size_name,
-                        //                 ];
-                        //                 // dd($aa_color);
-                        //                 array_push($aaa_size_list,$aas_size);
-                        //             }
-                        //             // dd($aaa_size_list);
-
-                        //             $aaa_color=[
-                        //                 'id'=>$color->color_id,
-                        //                 'code'=>$color->color_code,
-                        //                 'name'=>$color->color_name,
-                        //                 'sizes'=>$aaa_size_list
-                        //             ];
-                        //             array_push($aaa_color_list,$aaa_color);
-
-                        //             // dd($colors);
-
-                        //     }
                         $relation_type='warehouse_product';
                         $relation_id=$order_detail->warehouse_id;
                         $list_product = Products::find($warehouse_product->product_id);
@@ -345,8 +253,6 @@ class OrderController extends Controller
                                 "id"=>$warehouse_product->size_id,
                                 "name"=>$warehouse_product->size_name,
                             ]
-                            // "color_by_size"=>$size_list,
-                            // "size_by_color"=>$aaa_color_list
                         ];
 
                     }
@@ -397,11 +303,11 @@ class OrderController extends Controller
                     array_push($order_detail_list,$list);
                 }
                 $data=[
-                    'id'=>$order->id,
-                    'coupon_price'=>$order->coupon_price,
-                    'price'=>$order->price,
-                    'discount_price'=>$order->discount_price,
-                    'grant_total'=>$order->all_price,
+                    'id'=>$orderBasket->id,
+                    'coupon_price'=>$orderBasket->coupon_price,
+                    'price'=>$orderBasket->price,
+                    'discount_price'=>$orderBasket->discount_price,
+                    'grant_total'=>$orderBasket->all_price,
                     'list'=>$order_detail_list
                 ];
 
