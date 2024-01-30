0046645f3663a8dcfd5bc04663fee7d955b65330
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2024-01-30 11:21:29 +0500

    Order Kupon tuzatildi cupon tegishli bolgan order _ tovarlarigagina cupon beriladi

--- app/Constants.php
+++ app/Constants.php
@@ -11,19 +11,18 @@ class Constants {
    // Order    type_ids
    const BASKED = 1;
    const ORDERED = 2;
-   const ACCEPTED = 3;
-   const PERFORMED = 4;
-   const CANCELLED = 5;
-   const ACCEPTED_BY_RECIPIENT = 6;
-   // 1 basked 2 ordered 3 accepted 4 on_the_way 5 finished
+   const PERFORMED = 3;
+   const CANCELLED = 4;
+   const ACCEPTED_BY_RECIPIENT = 5;
+   // 1 basked 2 ordered 3 performed 4 cancelled 5 accepted by recipient
 
    // Order    payment_method
    const CASH_ON_DELIVERY = 0;
    const BANK_CARD = 1;
 
    // coupon type
-   const TO_ORDER_COUNT = 0; //maslan 10 ta ordarga
-   const FOR_ORDER_NUMBER = 1; // masalan 10 - ordarga  // agar typy null bosa demak coupon order countga berilmagan
+   const TO_ORDER_COUNT = 0; //maslan 10 ta orderga
+   const FOR_ORDER_NUMBER = 1; // masalan 10 - orderga  // agar typy null bosa demak coupon order countga berilmagan
 
     //Personal info
     const MALE = 1;
