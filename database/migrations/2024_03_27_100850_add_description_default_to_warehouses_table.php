d233a22219430046dc55b8c7fa11eeded98b3ab6
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2024-03-27 15:17:00 +0500

    Warehouse description va Material name default null qilindi

---
+++ database/migrations/2024_03_27_100850_add_description_default_to_warehouses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('warehouses', function (Blueprint $table) {
+            $table->text('description')->default(NULL)->change();
+        });
+        Schema::table('materials', function (Blueprint $table) {
+            $table->text('name')->default(NULL)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('warehouses', function (Blueprint $table) {
+            //
+        });
+    }
+};
