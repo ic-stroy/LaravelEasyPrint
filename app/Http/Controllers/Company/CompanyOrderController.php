4665261a72350b4eaa73d421d4b21c295f2282ce
Elyor12345678 <elyoreasy@gmail.com>
2024-05-17 10:21:33 +0500

    test

--- app/Http/Controllers/Company/CompanyOrderController.php
+++ app/Http/Controllers/Company/CompanyOrderController.php
@@ -144,7 +144,7 @@ class CompanyOrderController extends Controller
                     $products[] = [$order_detail, $order_detail_all_price, 'images'=>$images,
                         'discount_withouth_expire'=>$discount_withouth_expire, 'product_discount_withouth_expire'=>$product_discount_withouth_expire
                     ];
-                }elseif(!$order_detail->warehouse_id && $order_detail->product_id){
+                }elseif($order_detail->product_id){
                     $product_types = $product_types + 1;
                     $order_has = true;
                     $uploads=Uploads::where('relation_type', Constants::PRODUCT)->where('relation_id', $order_detail->product_id)->get();
