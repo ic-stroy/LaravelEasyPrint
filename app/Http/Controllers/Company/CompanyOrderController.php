92d30ac5d028633c809c7ea4d8d758c17c4713a4
Elyor12345678 <elyoreasy@gmail.com>
2024-05-17 10:28:43 +0500

    test uploads

--- app/Http/Controllers/Company/CompanyOrderController.php
+++ app/Http/Controllers/Company/CompanyOrderController.php
@@ -147,7 +147,9 @@ class CompanyOrderController extends Controller
                 }elseif(!$order_detail->warehouse_id && $order_detail->product_id){
                     $product_types = $product_types + 1;
                     $order_has = true;
-                    $uploads=Uploads::where('relation_id', $order_detail->product_id)->get();
+//                    $uploads=Uploads::where('relation_type', Constants::PRODUCT)->where('relation_id', $order_detail->product_id)->get();
+                    $uploads=Uploads::all();
+                    dd($order_detail->product_id, $uploads);
                     if($order_detail->status == Constants::ORDER_DETAIL_PERFORMED) {
                         $performed_product_types = $performed_product_types + 1;
                         $performed_company_product_price = $performed_company_product_price + $order_detail->price * $order_detail->quantity - $order_detail->discount_price;
@@ -212,8 +214,6 @@ class CompanyOrderController extends Controller
                     $product_discount_withouth_expire = !empty($order_detail->product->discount_whithout_expire)?$order_detail->product->discount_whithout_expire->percent:0;
                     $products_with_anime[] = [$order_detail, $order_detail_all_price, $products_with_anime_uploads,
                         'images'=>$images, 'product_discount_withouth_expire'=>$product_discount_withouth_expire];
-                    dd($uploads);
-
                 }
             }
             if((int)$order->coupon_price>0){
