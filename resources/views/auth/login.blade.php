7bea7bc871094fedb8b3f9e54f961be024cb17bc
Elyor12345678 <elyoreasy@gmail.com>
2024-06-06 16:30:41 +0500

    login page tuzatildi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.app')
 
 @section('content')
+    <link href="https://fonts.cdnfonts.com/css/gilroy-bold" rel="stylesheet">
     <style>
         .white_color{
             color: white;
@@ -41,22 +42,34 @@
             color: white;
         }
 
-        .login_modal_background {
-            width: 503px;
-        }
         .login_modal{
-            width: 380px;
+            width: 334px;
         }
         .login_modal_background{
+            width: 424px;
+            height: 424px;
             display: flex;
             justify-content: center;
             background-image: url("{{asset('assets/images/border_back.svg')}}");
             background-size: contain;
             background-repeat: no-repeat;
+            font-family: Gilroy !important;
         }
         .login_modal{
             color: white;
             padding: 37px 0px;
+        }
+        .login_modal h3{
+            font-family: Gilroy !important;
+            font-weight: 700;
+            font-size: 28px;
+            line-height: 46px;
+        }
+        .login_modal label, .login_modal a{
+            font-family: Gilroy !important;
+            font-weight: 400;
+            font-size: 14px;
+            line-height: 20px;
         }
         .color_white{
             color: white !important;
@@ -64,13 +77,19 @@
         .sign_in_button{
             background-color: #003465;
             border: 0px;
-            border-radius: 2px;
+            border-radius: 8px !important;
+            font-weight: 700;
             width: 100%;
             color: white;
             height: 38px;
         }
         .form-check-input:checked{
             background-color: #71B6F9 !important;
+        }
+        .eye_icon{
+            padding-right: 14px;
+            color: black;
+            margin-top: -28px;
         }
     </style>
     <div class="login_page">
@@ -83,56 +102,72 @@
             <div class="login_modal_background">
                 <div class="login_modal">
                     <img src="{{asset('assets/images/easyprint.svg')}}" alt="" height="40px">
-                    <div class="text-start mt-2 mb-3">
+                    <div class="text-start">
                         <h3 class="mt-0 color_white">Войти</h3>
                     </div>
                     <form action="{{route('login')}}" method="POST">
                         @csrf
                         <div class="mb-3">
-                            <label for="emailaddress" class="form-label">Адрес электронной почты</label>
-                            <input class="form-control @error('email') is-invalid @enderror" type="email" id="emailaddress" name="email" required="" placeholder="Введите адрес электронной почты">
+                            <label for="emailaddress" class="form-label">Эмаил</label>
+                            <input class="form-control @error('email') is-invalid @enderror" type="email" id="emailaddress" name="email" required="" placeholder="username@gmail.com">
                         </div>
                         @error('email')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
-                        <div class="mb-3">
+                        <div class="mb-1">
                             <label for="password" class="form-label">Пароль</label>
-                            <input class="form-control @error('password') is-invalid @enderror" type="password" required="" name="password" id="password" placeholder="Введите ваш пароль">
+                            <input class="form-control @error('password') is-invalid @enderror" type="password" required="" name="password" id="password" placeholder="Password">
+                            <div class="d-flex justify-content-between">
+                                <div></div>
+                                <div class="eye_icon hide_password"><i id="hide_password_icon" class="fa fa-eye"></i></div>
+                                <div class="eye_icon show_password d-none"><i id="show_password_icon" class="fa fa-eye-slash"></i></div>
+                            </div>
                         </div>
                         @error('password')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
-                        <div class="mb-3">
-                            <div class="form-check d-flex correct_img justify-content-between" style="padding-left:0px;">
-                                {{-- captcha_img('flat'); --}}
-                                {!! captcha_img('flat') !!}
-                                {{-- {!!  !!} --}}
-                                <input style="width:47%; margin-left:20px;" class="form-control" type="text" name="captcha" id="captcha" required>
-                            </div>
-                        </div>
-                        @error('captcha')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                        <div class="mb-3 d-flex justify-content-between">
-                            <a href="{{ route('password.request') }}" class="text-muted ms-1 color_white"><i class="fa fa-lock me-1"></i>Забыли пароль?</a>
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="checkbox-signin" checked>
-                                <label class="form-check-label" for="checkbox-signin">Запомнить меня</label>
-                            </div>
+                        <div class="mb-3 d-flex justify-content-start">
+                            <a href="{{ route('password.request') }}" class="text-muted ms-1 color_white">Забыли пароль?</a>
                         </div>
                         <button class="sign_in_button">Войти</button>
                     </form>
                 </div>
 
-
             </div> <!-- end card-body -->
             <!-- end card -->
         </div> <!-- end col -->
     </div>
+    <script>
+        let hide_password = document.getElementsByClassName('hide_password')
+        let password = document.getElementById('password')
+        let show_password = document.getElementsByClassName('show_password')
+        let hide_password_icon = document.getElementById('hide_password_icon')
+        let show_password_icon = document.getElementById('show_password_icon')
+        hide_password_icon.addEventListener('click', function () {
+            if(!hide_password[0].classList.contains('d-none')){
+                hide_password[0].classList.add('d-none')
+            }
+            if(show_password[0].classList.contains('d-none')){
+                show_password[0].classList.remove('d-none')
+            }
+            if(password.getAttribute('type') && password.getAttribute('type') == 'password'){
+                password.setAttribute('type', 'text')
+            }
+        })
+        show_password_icon.addEventListener('click', function () {
+            if(!show_password[0].classList.contains('d-none')){
+                show_password[0].classList.add('d-none')
+            }
+            if(hide_password[0].classList.contains('d-none')){
+                hide_password[0].classList.remove('d-none')
+            }
+            if(password.getAttribute('type') && password.getAttribute('type') == 'text'){
+                password.setAttribute('type', 'password')
+            }
+        })
+    </script>
 @endsection
