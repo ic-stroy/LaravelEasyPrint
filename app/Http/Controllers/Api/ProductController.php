2160f68d4e3d8619b788641f1e44a38f99fa20c5
Elyor12345678 <elyoreasy@gmail.com>
2024-06-06 14:28:35 +0500

    test komment qilindi

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -137,12 +137,10 @@ class ProductController extends Controller
                 'images' => $this->getImages($product_, 'product')
             ];
         }
-        $warehouse_products = array_merge($warehouse_products,$warehouse_products,$warehouse_products,
-$warehouse_products,$warehouse_products,$warehouse_products,$warehouse_products,$warehouse_products,$warehouse_products,
-$warehouse_products,$warehouse_products,$warehouse_products);
-//        $products = array_merge($products,$products,$products,
-//            $products,$products,$products,$products,$products,$products,
-//            $products,$products,$products);
+//        $warehouse_products = array_merge($warehouse_products,$warehouse_products,$warehouse_products,
+//$warehouse_products,$warehouse_products,$warehouse_products,$warehouse_products,$warehouse_products,$warehouse_products,
+//$warehouse_products,$warehouse_products,$warehouse_products);
+
 
         $data = [
             'product_list' => $products,
