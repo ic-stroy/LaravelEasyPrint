d0d50c37caef77f787871c04644ed5314e12cda3
Elyor12345678 <elyoreasy@gmail.com>
2024-07-04 15:01:17 +0500

    test

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -349,7 +349,8 @@ class OrderController extends Controller
                             }
 
                             $translate_name = table_translate($warehouse_product, 'warehouse', $language);
-//                            return response()->json([$warehouse_product, $translate_name, $warehouse_product->warehouse_product_name]);
+                            $warehouse__ = Warehouse::find($warehouse_product->warehouse_product_id);
+                            return response()->json([$warehouse_product, $translate_name, $warehouse_product->warehouse_product_name, $warehouse__]);
                             if (!$translate_name) {
                                 $product_ = Products::find($warehouse_product->product_id);
                                 $translate_name = table_translate($product_, 'product', $language);
