93752111f0363d48029382b8c1c3a677a36fdadf
Elyor12345678 <elyoreasy@gmail.com>
2024-07-08 16:32:58 +0500

    discount yo'q bo'lsa all_price null jo'natilmoqda

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -649,7 +649,7 @@ class OrderController extends Controller
                             }else{
                                 $images = [$order_detail_image_front, $order_detail_image_back];
                             }
-
+                            $
                             $list=[
                                 "id"=>$order_detail->id,
                                 "relation_type"=>$relation_type,
@@ -657,7 +657,7 @@ class OrderController extends Controller
                                 'name'=>$product->product_name,
                                 "price"=>$order_detail->price,
                                 "discount"=>$order_detail->discount,
-                                "all_price"=>(int)$order_detail->price - (int)$order_detail->discount_price,
+                                "all_price"=>(int)$order_detail->discount_price>0?(int)$order_detail->price - (int)$order_detail->discount_price:null,
                                 "quantity"=>$order_detail->quantity,
                                 "description"=>$product->description,
                                 "images"=>$images,
