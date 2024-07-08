46c8bb3a43b18f1cb1dd6e559f4b9e92b0535274
Elyor12345678 <elyoreasy@gmail.com>
2024-07-08 15:46:44 +0500

    test

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -272,8 +272,8 @@ class CategoryController extends Controller
         if(isset($token) && $token){
             $client = new \GuzzleHttp\Client();
             $url ='https://'.$_SERVER['HTTP_HOST'];
-            dd($url);
             $guzzle_request = new GuzzleRequest('GET', $url.'/api/user-info');
+            dd($guzzle_request);
             try{
                 $res = $client->sendAsync($guzzle_request, $options)->wait();
                 $result = $res->getBody();
